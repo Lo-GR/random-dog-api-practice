eca65732842329672d55fdcd910da202e79f0b3c
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-16 09:32:44 -0700

    create button for foxes

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,13 @@ import '../styles/App.css';
 import React, { useState } from 'react'
 
 function App() {
-  const [foxes, setFoxes] = useState([])
+  const [foxes, setFoxes] = useState([]);
   return (
     <div className="App">
       <header className="App-header">
+        <div className="clickHere">
+          <p>Click Here for Foxes</p>
+        </div>
         {foxes.map(fox => <img  src={fox} alt="a cute fox"/>)}
       </header>
     </div>
