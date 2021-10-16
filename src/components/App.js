d7ff81697cadcde0d0e65a9255bbf2a082a2c293
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-16 09:30:31 -0700

    add logic for displaying fox image

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import '../styles/App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [foxes, setFoxes] = useState([])
   return (
     <div className="App">
       <header className="App-header">
-        
+        {foxes.map(fox => <img  src={fox} alt="a cute fox"/>)}
       </header>
     </div>
   );
