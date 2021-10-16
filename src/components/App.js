a583fcc5fb71d10350d2c7bde1ed625f0360cc89
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-16 09:44:43 -0700

    add WIP logic for fox retrieval

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import '../styles/App.css';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
   const [foxes, setFoxes] = useState([]);
+  const useGetFoxes = () =>{
+    useEffect(() => {
+      fetch("https://randomfox.ca/floof")
+        .then(response => response.json())
+        .then(data => setFoxes([...foxes, data.image]))
+    }, [])
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <div className="clickHere">
+        <div className="clickHere" onClick={useGetFoxes}>
           <p>Click Here for Foxes</p>
         </div>
         {foxes.map(fox => <img  src={fox} alt="a cute fox"/>)}
