bc44f5cd88772b338493d3c0a03d08ff129014ae
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-18 17:53:26 -0700

    add video check and amend for doggo api

--- src/components/App.js
+++ src/components/App.js
@@ -2,37 +2,42 @@ import '../styles/App.css';
 import React, { useState } from 'react'
 
 function App() {
-  const [foxes, setFoxes] = useState([]);
-
+  const [dogs, setDogs] = useState([]);
+  const checkFileType = (url) =>{
+    if(url.endsWith(".mp4") || url.endsWith(".webm")){
+      return true
+    }
+    return false;
+  }
   async function handleGetFox(){
-    await fetch("https://aws.random.cat/meow", {
+    await fetch("https://random.dog/woof.json", {
         method: "GET",
       })
-      // .catch(err => {
-      //   console.log(err)
-      // })
-    // console.log(foxResponse);
-    // const foxData = await foxResponse.json();
-    // setFoxes([...foxes, foxData.image])
       .then(response => response.json())
-      .then(data => setFoxes([...foxes, data.file]))
+      .then(data => setDogs([...dogs, data.url]))
       .catch(err => {
         console.log(err)
       })
   }
   const check = () =>{
-    console.log(foxes);
+    console.log(dogs);
   }
   return (
     <div className="App">
       <header className="App-header">
         <div className="clickHere" onClick={handleGetFox}>
-          <p>Click Here for Foxes</p>
+          <p>Click Here for Dogs</p>
         </div>
         <div className="clickHere" onClick={check}>
           <p>check</p>
         </div>
-        {foxes.map(fox => <img  src={fox} alt="a cute fox"/>)}
+        {dogs.map(dog => {
+          if(checkFileType(dog)){
+            return <video src={dog} width="320" height="240" type="video/mp4" controls/>
+          } else {
+            return <img  src={dog} alt="a cute doggo"/>
+          }
+          })}
       </header>
     </div>
   );
